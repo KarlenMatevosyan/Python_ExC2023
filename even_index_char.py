644b81c8ccc504f72b19d3d1d7a3fa2a3ec321fc
Karlen <karlen.matevosyan.ec@gmail.com>
2023-11-28 23:35:09 +0400

    Print even index characters from given string

---
+++ even_index_char.py
@@ -0,0 +1,8 @@
+'''
+DOC
+The program prints the characters whose indexes are even numbers
+'''
+
+input_string = input("Enter the string: ")  # input
+print("Original string -", input_string)  # print original string
+print("Printing even indexes -", input_string[::2])  # print string with 2 step
